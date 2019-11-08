9e8050e3176ce636b70b90e72dbcaec0c9fbbe95
”竹森　諒” <azuryo0701@gmail.com>
2019-11-08 14:19:46 +0000

    second commit

--- php_practice/practice.php
+++ php_practice/practice.php
@@ -2,18 +2,22 @@
 $a = 3;
 $b = 7;
 echo $a + $b;
+echo "\n";
 
 $array = ["1月","2月","3月","4月","5月","6月","７月","8月"];
 echo $array[7];
+echo "\n";
 
 $hello = "HELLO,";
 $name = "Takemori";
 $world = "'s World!";
 echo $hello.$name.$world;
+echo "\n";
 
 $tech_boost = "tech";
 $tech_boost .= " boost";
 echo $tech_boost;
+echo "\n";
 
 $calendar_2018 = [
   "January" => "1月",
@@ -32,3 +36,4 @@ $calendar_2018 = [
 
 // 12月を表示する
 echo $calendar_2018["December"];
+echo "\n";

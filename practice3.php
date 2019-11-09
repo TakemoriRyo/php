304ecb23a86831c405d3cd9c758678bcbf5954d3
”竹森　諒” <azuryo0701@gmail.com>
2019-11-09 15:25:58 +0000

    sixthcommit

--- practice3.php
+++ practice3.php
@@ -1,40 +1,49 @@
-<?php  
-$a = 3;
-$b = 7;
-echo $a + $b;
+<?php
+function double($number){
+    return $number * 2;
+}
+echo double(10);
 echo "\n";
 
-$array = ["1月","2月","3月","4月","5月","6月","７月","8月","9月","10月","11月","12月"];
-echo $array[7];
+function total($a, $b){
+    return $a + $b;
+}
+echo total(5, 5);
 echo "\n";
 
-$hello = "HELLO,";
-$name = "Takemori";
-$world = "'s World!";
-echo $hello.$name.$world;
+$arr = array(1,3,5,7,9);
+echo array_product($arr);
 echo "\n";
 
-$tech_boost = "tech";
-$tech_boost .= " boost";
-echo $tech_boost;
+$max = 0;
+function max_array($arr){
+    $max_number = $arr[0];
+    foreach($arr as $a){
+        if ($max_number < $a){
+            $max_number = $a;
+        }
+    }
+    return $max_number;
+}
+echo max_array($arr);
 echo "\n";
 
-$calendar_2018 = [
-  "January" => "1月",
-  "February" => "2月",
-  "March" => "3月",
-  "April" => "4月",
-  "May" => "5月",
-  "June" => "6月",
-  "July" => "7月",
-  "August" => "8月",
-  "September" => "9月",
-  "October" => "10月",
-  "November" => "11月",
-  "December" => "12月"
-];
-
-// 12月を表示する
-echo $calendar_2018["December"];
+$tag = "<h1>こんにちは</h1>"."<p>こんばんは</p>";
+echo strip_tags($tag,"<p>");
 echo "\n";
+
+array_push($arr,11,13,15);
+print_r($arr);
+
+$second = array(2,4,6,8,10,12,14);
+$array = array_merge($arr,$second);
+echo asort($array);
+print_r($array);
+echo"\n";
+
+echo time();
+echo "\n";
+
+date_default_timezone_set('UTC');
+echo date("y-m-d-l", mktime(0, 0, 0, 11, 9, 2019));
 ?>
